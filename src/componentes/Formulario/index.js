91a1c4d63d3656dd342b47bc3d6b2d3cedaef9d3
Maria Paula <mpsolia@gmail.com>
2025-01-27 08:48:17 -0300

    add add novo time

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import Botao from "../Botao";
-import CampoTexto from "../CampoTexto";
+import Campo from "../Campo";
 import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
-const Formulario = ({ cadastrarTime, aoCadastrar, times }) => {
+const Formulario = ({ aoCadastrar, times, aoCriarTime }) => {
   const [nome, setNome] = useState("");
   const [cargo, setCargo] = useState("");
   const [imagem, setImagem] = useState("");
@@ -27,27 +27,27 @@ const Formulario = ({ cadastrarTime, aoCadastrar, times }) => {
     <section className="formulario-container">
       <form className="formulario" onSubmit={aoSubmeter}>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto
-          obrigatorio
+        <Campo
+          obrigatorio={true}
           label="Nome"
           placeholder="Digite seu nome "
           valor={nome}
           aoAlterado={(valor) => setNome(valor)}
         />
-        <CampoTexto
-          obrigatorio
+        <Campo
+          obrigatorio={true}
           label="Cargo"
           placeholder="Digite seu cargo "
           valor={cargo}
           aoAlterado={(valor) => setCargo(valor)}
         />
-        <CampoTexto
+        <Campo
           label="Imagem"
           placeholder="Informe o endereço da imagem "
           aoAlterado={(valor) => setImagem(valor)}
         />
         <ListaSuspensa
-          obrigatorio
+          obrigatorio={true}
           label="Times"
           items={times}
           valor={time}
@@ -55,30 +55,30 @@ const Formulario = ({ cadastrarTime, aoCadastrar, times }) => {
         />
         <Botao texto="Criar card" />
       </form>
-
       <form
         className="formulario"
-        onSubmit={(e) => {
-          e.preventDefault();
-          cadastrarTime({ nome: nomeTime, corTime });
+        onSubmit={(evento) => {
+          evento.preventDefault();
+          aoCriarTime({ nome: nomeTime, cor: corTime });
         }}
       >
         <h2>Preencha os dados para criar um novo time.</h2>
-        <CampoTexto
-          obrigatorio
+        <Campo
+          obrigatorio={true}
           label="Nome"
           placeholder="Digite o nome do time"
           valor={nomeTime}
           aoAlterado={(valor) => setNomeTime(valor)}
         />
-        <CampoTexto
-          obrigatorio
+        <Campo
+          obrigatorio={true}
           label="Cor"
-          placeholder="Digite a cor do time"
+          type="color"
+          placeholder="Digite sua cor"
           valor={corTime}
           aoAlterado={(valor) => setCorTime(valor)}
         />
-        <Botao texto="Criar time" />
+        <Botao texto="Criar Time" />
       </form>
     </section>
   );
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -2,7 +2,7 @@ import Colaborador from "../Colaborador";
 import "./Time.css";
 import hexToRgba from "hex-to-rgba";
 
-const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
+const Time = ({ time, colaboradores, aoDeletar, mudarCor, aoFavoritar }) => {
   return (
     colaboradores.length > 0 && (
       <section
@@ -13,23 +13,24 @@ const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
         }}
       >
         <input
-          value={time.cor}
           type="color"
           className="input-cor"
-          onChange={(e) => mudarCor(e.target.value, time.id)}
+          value={time.cor}
+          onChange={(evento) => {
+            mudarCor(evento.target.value, time.id);
+          }}
         />
         <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
         <div className="colaboradores">
-          {colaboradores.map((colaborador, indice) => {
-            return (
-              <Colaborador
-                key={indice}
-                colaborador={colaborador}
-                corDeFundo={time.cor}
-                aoDeletar={aoDeletar}
-              />
-            );
-          })}
+          {colaboradores.map((colaborador, indice) => (
+            <Colaborador
+              key={indice}
+              colaborador={colaborador}
+              corDeFundo={time.cor}
+              aoDeletar={aoDeletar}
+              aoFavoritar={aoFavoritar}
+            />
+          ))}
         </div>
       </section>
     )
